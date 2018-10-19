51fd5329bfefddb4b50260c91ac8ba3b62a39a8f
GApostolidis <GApostolidis@users.noreply.github.com>
2018-10-20 00:24:09 +0300

    Make LoginActivity First to Start

--- app/src/main/java/com/taskboss/euc/taskboss/LoginActivity.java
+++ app/src/main/java/com/taskboss/euc/taskboss/LoginActivity.java
@@ -1,15 +1,12 @@
 package com.taskboss.euc.taskboss;
 
+import android.graphics.Typeface;
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
 
