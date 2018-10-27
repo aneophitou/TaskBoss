f85b53071451a72a8af9f04fa11b9d59869b219d
GApostolidis <GApostolidis@users.noreply.github.com>
2018-10-27 18:27:44 +0300

    Activity fixes

--- app/src/main/java/com/taskboss/euc/taskboss/create_task.java
+++ app/src/main/java/com/taskboss/euc/taskboss/create_task.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class create_task extends AppCompatActivity {
 
@@ -25,6 +27,9 @@ public class create_task extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+
+        //ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this,R.layout.@values/strings,AssignToIndividualList);
+    }
 }
