38178c7a438f504bee855d0a2e9106ae600f1838
aneophitou <andreas-neophitou@hotmail.com>
2018-10-29 02:43:34 +0200

    Significant changes made
    Changed the main activity
    changed the way items work in the list
    added method for items to have value when you tap on them in the list
    added another fragment for events
    added another fragment for all tasks
    deleted old activities which were using presets

--- app/src/main/java/com/taskboss/euc/taskboss/TaskItem.java
+++ app/src/main/java/com/taskboss/euc/taskboss/TaskItem.java
@@ -6,25 +6,63 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TaskItem extends AppCompatActivity {
-
+String[] MEMBERS = {"Andreas", "Giannis", "Ahmed", "Ola","Assign this task"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_item);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        String title = getIntent().getStringExtra("title");
+        String dateString = getIntent().getStringExtra("date");
+        TextView txtTitle = (TextView) this.findViewById(R.id.txtTaskTitle);
+        txtTitle.setText(title);
+        TextView txtDate = (TextView) this.findViewById(R.id.txtDate);
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("dd/MM/YYYY").parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String formattedDate = new SimpleDateFormat("dd MM YYYY").format(date);
+        txtDate.setText(formattedDate);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView txtDescription = (TextView) this.findViewById(R.id.txtDescription);
+        txtDescription.setText(getIntent().getStringExtra("description"));
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerAssign);
+
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, MEMBERS){
+          @Override
+          public View getView(int position, View convertView, ViewGroup parent){
+              View view = super.getView(position, convertView, parent);
+              if(position ==getCount()){
+                  ((TextView) view.findViewById(android.R.id.text1)).setText("");
+                  ((TextView) view.findViewById(android.R.id.text1)).setHint(getItem(getCount()));
+
+              }
+              return view;
+          }
+          @Override
+            public int getCount(){
+              return super.getCount()-1;
+          }
+        };
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setSelection(spinnerAdapter.getCount());
+
+
+
     }
 
 }
--- app/src/main/java/com/taskboss/euc/taskboss/tasksFragment.java
+++ app/src/main/java/com/taskboss/euc/taskboss/tasksFragment.java
@@ -2,6 +2,7 @@ package com.taskboss.euc.taskboss;
 
 
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,17 +11,12 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import java.util.List;
-
-
-/**
- * A simple {@link Fragment} subclass.
- */
+import android.widget.TextView;
 
 public class tasksFragment extends Fragment {
-String[] TASKS = {"task 1", "task 2"};
-
+String[] TASKS = {"task 1", "task 2", "task3"};
+String[] DATES = {"31/10/2018", "05/11/2018","06/11/2018"};
+String[] DESCRIPTIONS = {"Get candy for Trick or Treat", "Get Some Sleep", "Doctors Appointment"};
 
     public tasksFragment() {
         // Required empty public constructor
@@ -32,13 +28,29 @@ String[] TASKS = {"task 1", "task 2"};
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView= inflater.inflate(R.layout.fragment_tasks, container, false);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, TASKS) ;;
-        ListView listView = (ListView) rootView.findViewById(R.id.taskList);
+            ListView listView = (ListView) rootView.findViewById(R.id.taskList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, TASKS) {
+
+            //code to change the color of the text in the list
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+                textView.setTextColor(getResources().getColor(R.color.TextColor));
+
+                return view;
+            }
+        };
         listView.setAdapter(adapter);
+
+        //code to handle clicks of the items in the list
         listView.setOnItemClickListener( new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id){
                 Intent intent = new Intent(getActivity(),TaskItem.class);
+                intent.putExtra("title", TASKS[position]);
+                intent.putExtra("date", DATES[position]);
+                intent.putExtra("description", DESCRIPTIONS[position]);
                 startActivity(intent);
             }
         });
