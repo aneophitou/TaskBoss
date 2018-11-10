a4579072b0ff21a2d351040a76fd2d02ed0e06cc
Ahmed <ae171092@students.euc.ac.cy>
2018-11-10 15:46:48 +0200

    more settings

--- app/src/main/java/com/taskboss/euc/taskboss/SettingsActivity.java
+++ app/src/main/java/com/taskboss/euc/taskboss/SettingsActivity.java
@@ -1,24 +1,50 @@
 package com.taskboss.euc.taskboss;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    public static class SettingsFragment extends PreferenceFragment
-    {
+    public static class SettingsFragment extends PreferenceFragment {
         @Override
-        public void onCreate(Bundle savedInstanceState)
-        {
+        public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_fragment);
         }
+    public static class NotifiFrag extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.notifi_frag);
+
+        }
+        public void changeSummary()
+        {
+            CheckBoxPreference c = (CheckBoxPreference) findPreference("switch_preference_1");
+
+            if (c.isChecked())
+            {
+                c.setSummary("Click to switch off");
+            }
+            else if (!c.isChecked())
+            {
+                c.setSummary("Click to switch on");
+            }
+        }
+
     }
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,10 +53,7 @@ public class SettingsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar7);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        getFragmentManager().beginTransaction().add(R.id.activity_prefs, new SettingsFragment()).commit();
-
-    }
+}
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
